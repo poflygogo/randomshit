84afe06e670e6dc5fc305a88afaca0b8b5d66bf1
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-08-05 23:40:51 +0800

    Create a040. 阿姆斯壯數.py

---
+++ zerojudge/01基礎題庫/a040. 阿姆斯壯數.py
@@ -0,0 +1,12 @@
+arm_num_list = []
+a, b = map(int,input().split())
+for i in range(a,b+1):
+    i_str = str(i)
+    i_length = len(i_str)
+    check = 0
+    for j in i_str:
+        check += int(j) ** i_length
+    if i == check:
+        arm_num_list.append(i)
+if len(arm_num_list) == 0: print('none')
+else:print(*arm_num_list)
