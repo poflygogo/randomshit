41eebf7bb1ba51c2f26bd9a1be7ee1717fa0ddae
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-08-05 14:12:58 +0800

    Create a104. 排序.py

---
+++ zerojudge/a104. 排序.py
@@ -0,0 +1,9 @@
+while True:
+    try:
+        none = input()
+        data = map(int,input().split())
+        data = list(data)
+        data.sort()
+        print(*data)
+    except EOFError:
+        break
