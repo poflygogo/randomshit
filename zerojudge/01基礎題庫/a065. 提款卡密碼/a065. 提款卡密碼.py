db80a929c366d33e6046e0b0f5ea796c786eb1c4
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-08-06 22:14:36 +0800

    Create a065. 提款卡密碼.py

---
+++ zerojudge/01基礎題庫/a065. 提款卡密碼/a065. 提款卡密碼.py
@@ -0,0 +1,7 @@
+code = input()
+password =''
+length = len(code)
+code_ascii = tuple(ord(i) for i in code)
+for i in range(length-1):
+    password += str(abs(code_ascii[i+1]-code_ascii[i]))
+print(password)
