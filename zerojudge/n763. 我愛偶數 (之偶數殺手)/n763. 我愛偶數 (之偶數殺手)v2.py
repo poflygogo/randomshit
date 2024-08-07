aa1cd8f9328d9398f6967fdef0139c9b8be4efe6
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-08-08 01:57:31 +0800

    Create n763. 我愛偶數 (之偶數殺手)v2.py

---
+++ zerojudge/n763. 我愛偶數 (之偶數殺手)/n763. 我愛偶數 (之偶數殺手)v2.py
@@ -0,0 +1,15 @@
+from _collections import deque
+
+def odd_killer(num_list):
+    queue = deque(num_list)
+    while len(queue) > 1:
+        num = queue.popleft()
+        if num % 2 == 0:
+            queue.popleft()
+        queue.append(num)
+    return queue[0]
+
+length = int(input())
+num_list = list(map(int, input().split()))
+
+print(odd_killer(num_list))
