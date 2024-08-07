23c05cc807d58d44cb0554b55c1b8989f68fb0c9
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-08-07 23:52:52 +0800

    Create a121. 質數又來囉v2.0.py

---
+++ zerojudge/01基礎題庫/a121. 質數又來囉/a121. 質數又來囉v2.0.py
@@ -0,0 +1,52 @@
+
+prime_num = (2, 3, 5, 7, 11, 13, 17, 19, 23,
+             29, 31, 37, 41, 43, 47, 53, 59,
+             61, 67, 71, 73, 79, 83, 89, 97)
+
+def prime_num_check(num):
+    if num <= 100:
+        return CheckLess1e2(num)
+    elif num <= 10000:
+        return CheckLess1e4(num)
+    else:return CheckMore1e4(num)
+
+def CheckLess1e2(num):
+    for i in prime_num:
+        if num == i:
+            return True
+        elif i > num:
+            return False
+            break
+    else:
+        return False
+
+def CheckLess1e4(num):
+    for i in prime_num:
+        if num % i == 0:
+            return False
+    else:
+        return True
+
+def CheckMore1e4(num):
+    if CheckLess1e4(num) == False:
+        return False
+    else:
+        for i in range(101, int(num**0.5)+1, 2):
+            if num % i == 0:
+                return False
+        return True
+
+a, b = map(int,input().split())
+
+count = 0
+if a < 5:
+    for num in range(a, 4):
+        if (num == 2 or num == 3) and num <= b: count += 1
+    a = 5
+
+for num in range(5, b+1, 6):
+    if prime_num_check(num):count += 1
+for num in range(7, b+1, 6):
+    if prime_num_check(num):count += 1
+
+print(count)
