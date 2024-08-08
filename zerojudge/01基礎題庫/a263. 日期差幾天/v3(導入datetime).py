6ef98fed9802b95d5025c376333c9e4442580cc0
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-08-09 00:32:11 +0800

    Create v3(導入datetime).py

---
+++ zerojudge/01基礎題庫/a263. 日期差幾天/v3(導入datetime).py
@@ -0,0 +1,8 @@
+from datetime import datetime,date
+
+while True:
+    try:
+        y1, m1, d1 = map(int,input().split())
+        y2, m2, d2 = map(int,input().split())
+        print(abs(date(y1, m1, d1) - date(y2, m2, d2)).days)
+    except EOFError:break
