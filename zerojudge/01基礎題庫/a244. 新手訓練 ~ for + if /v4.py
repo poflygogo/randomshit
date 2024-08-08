9aee0f78228c2b02cdaeab7ebe7dbc7f68339267
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-08-08 18:30:48 +0800

    Create v4.py

---
+++ zerojudge/01基礎題庫/a244. 新手訓練 ~ for + if /v4.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    if a == 1:print(b + c)
+    elif a == 2:print(b - c)
+    elif a == 3:print(b * c)
+    else:print(int(b / c))
