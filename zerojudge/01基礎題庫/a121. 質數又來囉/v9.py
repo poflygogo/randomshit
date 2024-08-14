9ef7f4616e6ed786215d00f75146f9e509b72bed
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-08-14 09:02:40 +0800

    Update v9.py

--- zerojudge/01基礎題庫/a121. 質數又來囉/v9.py
+++ zerojudge/01基礎題庫/a121. 質數又來囉/v9.py
@@ -1,4 +1,5 @@
 def check_prime(num:int)->int:
+    """確認一個大於1e4的數是否為質數"""
     num_sqrt = int(num ** 0.5) + 1 
     for prime in primes:
         if prime > num_sqrt:
@@ -20,6 +21,7 @@ while True:
     try:
         a, b = map(int, input().split())
         
+        # 所有數字都在質數表內, 直接查就好
         result = 0
         if b <= 10000:
             for prime in primes:
@@ -28,6 +30,7 @@ while True:
                 elif prime > b:
                     break
 
+        # a <= 1e4 <= b, 先處理質數表範圍內的, 再處理範圍外的
         elif a <= 10000:
             a1 = a + (7 - a % 6) % 6
             a2 = a + 5 - a % 6
@@ -36,6 +39,7 @@ while True:
             result += sum(1 for num in range(10003, b + 1, 6) if check_prime(num))
             result += sum(1 for num in range(10001, b + 1, 6) if check_prime(num)) 
 
+        # a 和 b 都大於 1e4, 從 a 開始處理，到 b 結束
         else:
             a1 = a + (7 - a % 6) % 6
             a2 = a + 5 - a % 6
