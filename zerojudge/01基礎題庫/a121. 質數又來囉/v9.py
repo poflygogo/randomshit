68812f8edbaac1095e33f6c1efc3ec436c38d416
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-08-14 09:29:43 +0800

    Update v9.py

--- zerojudge/01基礎題庫/a121. 質數又來囉/v9.py
+++ zerojudge/01基礎題庫/a121. 質數又來囉/v9.py
@@ -39,7 +39,7 @@ while True:
             result += sum(1 for num in range(10003, b + 1, 6) if check_prime(num))  # 10003是1e4後第一個滿足6n+1的數
             result += sum(1 for num in range(10001, b + 1, 6) if check_prime(num))  # 10001是1e4後第一個滿足6n-1的數
 
-        # a 和 b 都大於 1e4, 從 a 開始處理，到 b 結束
+        # a 和 b 都大於 1e4, 從 a 開始處理, 到 b 結束
         else:
             a1 = a + (7 - a % 6) % 6
             a2 = a + 5 - a % 6
