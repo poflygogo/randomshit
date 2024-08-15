b8e77a4751b34b45418fbf6c887367646b126f32
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-08-15 23:36:13 +0800

    Create v1.py

---
+++ zerojudge/01基礎題庫/a225. 明明愛排列/v1.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        _ = input()
+    except EOFError:
+        break
+
+    data = list(map(int, input().split()))
+    data.sort(key= lambda num: (num%10, -num//10))
+    
+    print(*data)
