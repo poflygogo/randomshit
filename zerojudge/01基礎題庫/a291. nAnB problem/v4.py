8afd732534b8b83a4b1458af1ea31201ba425518
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-08-17 05:40:02 +0800

    Create v4.py

---
+++ zerojudge/01基礎題庫/a291. nAnB problem/v4.py
@@ -0,0 +1,29 @@
+from sys import stdin
+
+def count_nAnB(ans:str, guess:list) -> int:
+    ans = ans.split()
+    nA = 0
+    for i in range(4):
+        if guess[i] == ans[i]:
+            nA += 1
+            guess[i] = None
+            ans[i] = None
+
+    nB = 0
+    for num in guess:
+        if num != None and num in ans:
+            nB += 1
+            ans[ans.index(num)] = None
+    
+    return nA, nB
+
+
+for line in stdin:
+    if line.strip() == "":
+        continue
+    password = line
+
+    for _ in range(int(stdin.readline())):
+        test = stdin.readline().strip("\r\n").split()
+        nA, nB = count_nAnB(password, test)
+        print(f"{nA}A{nB}B")
