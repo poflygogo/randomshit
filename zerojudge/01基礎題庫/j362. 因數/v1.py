ba8c0823cdc118d102510446a86ed7fb8397eb92
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-08-17 06:54:55 +0800

    Update v1.py

--- zerojudge/01基礎題庫/j362. 因數/v1.py
+++ zerojudge/01基礎題庫/j362. 因數/v1.py
@@ -5,15 +5,10 @@ for i in range(1, int(num ** 0.5) + 1):
     if num % i == 0:
         output.extend((i, num//i))
 
+output.sort()
+
 if input() == "no":
     del output[0]
+    del output[-1]
 
-print(output)
-output.sort()
-print(output)
-
-print(
-    *output,
-    f"{num}的因數個數是{len(output)}，{num}的因數總和是{sum(output)}",
-    sep="\n"
-)
+print(*output, f"{num}的因數的個數是{len(output)}，{num}的因數的總和是{sum(output)}", sep="\n")
