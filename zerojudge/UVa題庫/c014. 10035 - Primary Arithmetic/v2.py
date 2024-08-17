a80024c5468c4764c1182032ed7d7fa9509a2e84
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-08-17 08:37:51 +0800

    Create v2.py

---
+++ zerojudge/UVa題庫/c014. 10035 - Primary Arithmetic/v2.py
@@ -0,0 +1,31 @@
+def carry_operation(listA:list, listB:list) -> int:
+    count = 0
+    for index, item in enumerate(listA):
+        if (item + listB[index]) // 10 != 0:
+            count += 1
+            if len(listA)-1 == index:
+                break
+            else:
+                listA[index+1] += 1
+    return count
+
+
+while True:
+    a, b = input().split()
+    if a == b == '0':
+        break
+
+    list_a = list(reversed([int(i) for i in a]))
+    list_b = list(reversed([int(i) for i in b]))
+
+    if len(list_a) <= len(list_b):
+        count = carry_operation(list_a, list_b)
+    else:
+        count = carry_operation(list_b, list_a)
+
+    if count == 0:
+        print(f'No carry operation.')
+    elif count == 1:
+        print(f'1 carry operation.')
+    else:
+        print(f'{count} carry operations.')
