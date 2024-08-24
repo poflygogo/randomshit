8fe3fcc5be9a4855e1e6ce7b2e294774fa7bcba0
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-08-24 18:12:28 +0800

    update

--- 01zerojudge/01 基礎題庫/series_b/b893. 勘根定理/v3.py
+++ 01zerojudge/01 基礎題庫/series_b/b893. 勘根定理/v3.py
@@ -1,17 +1,26 @@
 from sys import stdin, stdout
 
 def zi_func(n:int) -> int:
+    """
+    可能經常重複執行的的語句就定義成函數
+    求 f(x) 的值
+    """
     return (a * n ** 5) + (b * n ** 4) + (c * n ** 3) + (d * n ** 2) + (e * n) + f
 
 a, b, c, d, e, f = map(int, stdin.readline().strip().split())
 
+# 若所有值都是 0 ，代表與x軸重合，有無限多解
+# 若除了f外所有值都是0，代表與x軸平行，無解
 if a == b == c == d == e == 0:
     if f:
         stdout.write(r'N0THING! >\\\<')
     else:
         stdout.write('Too many... = ="')
 else:
-    result = []
+    result = []     # 把結果存起來，後面一起送出去
+
+    # 勘根定理
+    # 用 zip() 搭配 range() 取要傳入的值
     for i, j in zip(range(-35, 35), range(-34, 36)):
         func_i = zi_func(i)
         func_j = zi_func(j)
@@ -22,6 +31,7 @@ else:
             if func_i == 0:
                 result.append(f'{i} {i}')
 
+    # 如果全部循環完，result 裡面仍是空的，代表無解
     if result:
         result = '\n'.join(result)
         stdout.write(result)
