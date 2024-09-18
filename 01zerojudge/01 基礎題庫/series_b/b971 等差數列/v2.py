98cd739cf41f44dbeb52cfdcf32797635066e070
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-09-19 05:33:06 +0800

    update

--- 01zerojudge/01 基礎題庫/series_b/b971 等差數列/v2.py
+++ 01zerojudge/01 基礎題庫/series_b/b971 等差數列/v2.py
@@ -2,9 +2,9 @@ a1, an, diff = map(int, input().split())
 
 if diff > 0:
     for i in range(a1, an+1, diff):
-        print(i,end=" ")
+        print(i, end=" ")
 elif diff < 0:
     for i in range(a1, an-1, diff):
-        print(i,end=" ")
+        print(i, end=" ")
 else:
     print(a1)
