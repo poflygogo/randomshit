2cad953b933888dbda3091fd7f437df8c5d11b3a
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-09-26 22:04:50 +0800

    finish the code

--- 01zerojudge/01 基礎題庫/series_d/d299 程式設計師的面試問題/te.py
+++ 01zerojudge/01 基礎題庫/series_d/d299 程式設計師的面試問題/te.py
@@ -1,2 +1,15 @@
-num = ['', '', '', '', '', '', '', '', '', '']
+from itertools import permutations
+
+
 char = ['F', 'O', 'R', 'T', 'Y', 'E', 'N', 'S', 'I', 'X']
+for i in permutations(char):
+    ans = {j:i for i, j in enumerate(i)}
+    if ans['F'] == 0 or ans['S'] == 0:
+        continue
+
+    forty = sum((ans['F'] * 10000, ans['O'] * 1000, ans['R'] * 100, ans['T'] * 10, ans['Y']))
+    ten = sum((ans['T'] * 100, ans['E'] * 10, ans['N']))
+    sixty = sum((ans['S'] * 10000, ans['I'] * 1000, ans['X'] * 100, ans['T'] * 10, ans['Y']))
+
+    if forty + ten * 2 == sixty:
+        print(f'{forty} + {ten} + {ten} = {sixty}')
