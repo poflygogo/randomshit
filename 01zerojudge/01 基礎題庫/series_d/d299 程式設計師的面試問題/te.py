501ef4ae288673db4cf49880644eb611a64d9ed9
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-09-26 22:13:27 +0800

    add comment

--- 01zerojudge/01 基礎題庫/series_d/d299 程式設計師的面試問題/te.py
+++ 01zerojudge/01 基礎題庫/series_d/d299 程式設計師的面試問題/te.py
@@ -1,10 +1,10 @@
 from itertools import permutations
 
 
-char = ['F', 'O', 'R', 'T', 'Y', 'E', 'N', 'S', 'I', 'X']
-for i in permutations(char):
-    ans = {j:i for i, j in enumerate(i)}
-    if ans['F'] == 0 or ans['S'] == 0:
+char = ['F', 'O', 'R', 'T', 'Y', 'E', 'N', 'S', 'I', 'X']   # 把所有元素都放到一個長度為 10 的 list 中
+for i in permutations(char):                                # 直接對其進行排列組合，暴搜，窮舉
+    ans = {j:i for i, j in enumerate(i)}                    # 用字典生成式建立對照表
+    if ans['F'] == 0 or ans['S'] == 0:                      # 數字開頭絕對不可能是 0，遇到就直接略過
         continue
 
     forty = sum((ans['F'] * 10000, ans['O'] * 1000, ans['R'] * 100, ans['T'] * 10, ans['Y']))
@@ -13,3 +13,4 @@ for i in permutations(char):
 
     if forty + ten * 2 == sixty:
         print(f'{forty} + {ten} + {ten} = {sixty}')
+        break
