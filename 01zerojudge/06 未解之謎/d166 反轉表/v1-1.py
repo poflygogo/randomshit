215f817861ad0c6b59c41eac82f7441bf233de9c
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-09-26 15:57:43 +0800

    update

---
+++ 01zerojudge/06 未解之謎/d166 反轉表/v1-1.py
@@ -0,0 +1,17 @@
+from sys import stdin
+
+for line in stdin:
+    line = [int(i) for i in line.strip().split()]
+    if line == -1: break
+
+    result = [0] * len(line)
+    for num, item in enumerate(line):
+        count = 0
+        for i, j in enumerate(result):
+            if j == 0 and count == item:
+                result[i] = num + 1
+                break
+            if j == 0 or j > num:
+                count += 1
+
+    print(*result)
