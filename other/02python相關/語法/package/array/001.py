05e52e651bc2d00d076f2d54831f0783f242851d
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-09-30 05:14:10 +0800

    update

---
+++ other/02python相關/語法/package/array/001.py
@@ -0,0 +1,12 @@
+from array import array
+
+
+# 建立一個有 10 個 1 的 array
+array1 = array('i', [1] * 10)
+
+# 建立另一個用來互動的 array
+array2 = array('i', [2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+
+# 不支援 numpy 的這種操作
+# result = array1 * array2
+# print(result)
