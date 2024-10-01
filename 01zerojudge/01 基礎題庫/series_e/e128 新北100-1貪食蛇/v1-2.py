a1e6f2bddd119cdc4100addba25a4efdf4233108
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-10-01 10:27:06 +0800

    update

---
+++ 01zerojudge/01 基礎題庫/series_e/e128 新北100-1貪食蛇/v1-2.py
@@ -0,0 +1,28 @@
+from math import sqrt
+
+
+while True:
+    n = int(input())
+    if n == 0: break
+
+    n_sqrt = sqrt(n)
+    if n_sqrt.is_integer():
+        if n_sqrt % 2:
+            print(1, int(n_sqrt))
+        else:
+            print(int(n_sqrt), 1)
+
+    else:
+        n_sqrt = int(n_sqrt)
+        mid = n_sqrt ** 2 + n_sqrt + 1
+        if n_sqrt % 2:
+            if n <= mid:
+                print(n - n_sqrt ** 2, n_sqrt + 1)
+            else:
+                print(n_sqrt + 1, (n_sqrt + 1) ** 2 - n + 1)
+
+        else:
+            if n <= mid:
+                print(n_sqrt + 1, n - n_sqrt ** 2)
+            else:
+                print((n_sqrt + 1) ** 2 - n + 1, n_sqrt + 1)
