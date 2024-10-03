e8c49c4fbba3817447664db8b23e1bc1fa6ac6cf
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-10-03 15:38:09 +0800

    pep8

--- 01zerojudge/01 基礎題庫/series_a/a216 數數愛明明/v1.py
+++ 01zerojudge/01 基礎題庫/series_a/a216 數數愛明明/v1.py
@@ -1,9 +1,11 @@
 while True:
-    try:day = int(input())
-    except EOFError:break
+    try:
+        day = int(input())
+    except EOFError:
+        break
     a = 0
     b = 0
-    for i in range(1, day+1):
+    for i in range(1, day + 1):
         a += i
         b += a
-    print(a,b)
+    print(a, b)
--- 01zerojudge/01 基礎題庫/series_a/a216 數數愛明明/v2.py
+++ 01zerojudge/01 基礎題庫/series_a/a216 數數愛明明/v2.py
@@ -1,10 +1,14 @@
-def loveFromMin(n):
+def love_from_min(n):
     return int(n * (n + 1) / 2)
 
-def loveFromShu(n):
-    return int(loveFromMin(n) * (n + 1) - n * (n + 1) * (2 * n + 1) / 6)
+
+def love_from_shu(n):
+    return int(love_from_min(n) * (n + 1) - n * (n + 1) * (2 * n + 1) / 6)
+
 
 while True:
-    try:day = int(input())
-    except EOFError:break
-    print(loveFromMin(day),loveFromShu(day))
+    try:
+        day = int(input())
+    except EOFError:
+        break
+    print(love_from_min(day), love_from_shu(day))
