ff93d4eeeb77c5fa80fcdd17b6b9f1851d912ff1
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-10-26 15:33:16 +0800

    update

---
+++ 01zerojudge/04 UVa題庫/c006 10550 - Combination Lock/v1-3.py
@@ -0,0 +1,11 @@
+while True:
+    line = [int(i) for i in input().split()]
+    if line == [0, 0, 0, 0]:
+        exit()
+
+    print(
+        1080 +                                                  # 360 * (2 + 1)
+        9 * (line[0] - line[1] + 40 * (line[1] > line[0]) +     # 順時鐘轉直到到達第 1 個號碼
+             line[2] - line[1] + 40 * (line[1] > line[2]) +     # 逆時鐘轉直到到達第 2 個號碼
+             line[2] - line[3] + 40 * (line[3] > line[2]))      # 順時鐘轉直到到達第 3 個號碼
+    )
