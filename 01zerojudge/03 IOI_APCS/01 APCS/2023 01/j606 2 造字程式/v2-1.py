67e080a31bd49ee33b3f68609cd700ad5c8e013c
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-10-31 17:42:25 +0800

    update

---
+++ 01zerojudge/03 IOI_APCS/01 APCS/2023 01/j606 2 造字程式/v2-1.py
@@ -0,0 +1,10 @@
+k, q, r = map(int, input().split())
+result = [[i for i in input()]]
+
+# 將字串根據輸入的條件重新排列，並將結果放在同一個 list 中
+for _ in range(q):
+    result.append([i[0] for i in sorted(zip(result[-1], input().split()), key=lambda x: int(x[1]))])
+
+# 輸出結果
+for i in range(r):
+    print(''.join(result[j][i] for j in range(1, q + 1)))
