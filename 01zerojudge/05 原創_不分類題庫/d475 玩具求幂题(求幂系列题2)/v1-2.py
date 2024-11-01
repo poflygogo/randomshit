95413e017561a0cc40ac419b13eb77ad683df7d4
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-11-01 15:35:49 +0800

    update

---
+++ 01zerojudge/05 原創_不分類題庫/d475 玩具求幂题(求幂系列题2)/v1-2.py
@@ -0,0 +1,12 @@
+from sys import stdin
+from decimal import Decimal
+ 
+
+for line in stdin:
+    a, n = map(Decimal, line.rstrip().split())
+    
+    if a == n == 0:
+        print(f'All Over. Exceeded 65536 lines!')
+        exit()
+
+    print(pow(a, n))
