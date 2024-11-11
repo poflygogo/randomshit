df07baf0b55a3075fbe26474e51bede916da1068
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-11-11 16:19:59 +0800

    update

---
+++ 01zerojudge/04 UVa題庫/d186 11461 - Square Numbers/v1-1.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+# UVa 11461 Square Numbers
+# Zerojudge d186
+
+from math import sqrt
+
+
+while True:
+    a, b = map(int, input().split())
+    if a == b == 0:
+        exit()
+    
+    a_sqrt, b_sqrt = sqrt(a), sqrt(b)
+    total = int(b_sqrt) - int(a_sqrt) + a_sqrt.is_integer()
+    print(total)
