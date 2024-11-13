0dc709e1fbff3d97e4e7e9ab51b5bbbe986cbe36
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-11-13 09:56:11 +0800

    update

---
+++ 01zerojudge/04 UVa題庫/d387 10235 - Simply Emirp/v1-1.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+# python 3.12
+# UVa 10235 Simply Emirp
+# ZeroJudge d387
+
+import random
+
+
+def is_prime(n, k=5):
+    if n < 2:
+        return False
+    if n in (2, 3):
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+
+    # Miller-Rabin
+    r, s = 0, n - 1
+    while s % 2 == 0:
+        r += 1
+        s = s >> 1
+
+    for _ in range(k):
+        a = random.randint(2, n - 1)
+        x = pow(a, s, n)
+        if x in (1, n - 1):
+            continue
+        for _ in range(r - 1):
+            x = x * x % n
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
+
+
+while True:
+    try:
+        num = int(input())
+        num_reverse = int(str(num)[::-1])
+        
+    except EOFError:
+        exit()
+
+    else:
+        print(
+            num,
+            'is',
+            'not prime.' if not is_prime(num) else
+            'emirp.' if num_reverse != num and is_prime(int(str(num)[::-1])) else
+            'prime.'
+        )
