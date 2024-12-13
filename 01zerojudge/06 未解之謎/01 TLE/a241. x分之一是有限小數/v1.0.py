b3e525c2347febfdc6cf614c9330f38632188787
poflygogo <144988024+poflygogo@users.noreply.github.com>
2024-12-13 13:25:43 +0800

    del some files

--- 01zerojudge/06 未解之謎/01 TLE/a241. x分之一是有限小數/v1.0.py
+++
@@ -1,4 +0,0 @@
-for _ in range(int(input())):
-    print(
-        sum([1 for i in range(2, int(input()) + 1) if i % 2 == 0 or i % 5 == 0])
-    )
--- 01zerojudge/06 未解之謎/01 TLE/a241. x分之一是有限小數/v2.1.py
+++
@@ -1,5 +0,0 @@
-for _ in range(int(input())):
-    num = int(input())
-    print(
-        num // 2 + num // 5 - num // 10 - num // 6
-    )
