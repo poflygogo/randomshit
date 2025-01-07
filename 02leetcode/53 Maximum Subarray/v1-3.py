e26cf71cd73e5cd9c6e905e397b3f413b86b2b49
poflygogo <144988024+poflygogo@users.noreply.github.com>
2025-01-07 22:27:05 +0800

    update

--- 02leetcode/53 Maximum Subarray/v1-3.py
+++ 02leetcode/53 Maximum Subarray/v1-3.py
@@ -2,20 +2,25 @@
 # python 3.12
 # LeetCode 53. Maximum Subarray
 
-# 前綴和，通過更多測資了，但還是 TLE
+# Kadane's Algorithm to Maximum Sum Subarray Problem | Youtube
+# https://www.youtube.com/watch?v=86CQq3pKSUw&t=666s
 
 
 from typing import List
 
 
-# class Solution:
-#     def maxSubArray(self, nums: List[int]) -> int:
-#         prefix_sum = [0] * (len(nums) + 1)
-#         for i in range(len(nums)):
-#             prefix_sum[i + 1] = prefix_sum[i] + nums[i]
-#         return max(prefix_sum[j] - prefix_sum[i] for i in range(len(nums)) for j in range(i + 1, len(nums) + 1))
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # Kadane's Algorithm
+        # dp
+        max_curr = max_global = nums[0]
+        for i in range(1, len(nums)):
+            max_curr = max(nums[i], max_curr + nums[i])
+            if max_curr > max_global:
+                max_global = max_curr
+        return max_global
 
 
-# if __name__ == '__main__':
-#     s = Solution()
-#     print(s.maxSubArray([-1]))
+if __name__ == '__main__':
+    s = Solution()
+    print(s.maxSubArray([-1]))
