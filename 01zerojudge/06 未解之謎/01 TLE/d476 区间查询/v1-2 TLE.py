f360e4a92adf67022bc4f549a800d8044c546091
poflygogo <144988024+poflygogo@users.noreply.github.com>
2025-01-14 01:21:47 +0800

    update

---
+++ 01zerojudge/06 未解之謎/01 TLE/d476 区间查询/v1-2 TLE.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+# python 3.12
+# ZeroJudge d476. 区间查询
+
+
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+for _ in range(m):
+    action, *args = input().split()
+    args = list(map(int, args))
+    if action == 'Q':
+        temp = nums[args[0] - 1:args[1]]
+        temp.sort()
+        print(temp[args[2] - 1])
+    else:
+        nums[args[0] - 1] = args[1]
