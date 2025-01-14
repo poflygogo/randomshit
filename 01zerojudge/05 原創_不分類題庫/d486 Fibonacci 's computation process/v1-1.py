98927e19eea09f892addb19cdfd901ca7c31f937
poflygogo <144988024+poflygogo@users.noreply.github.com>
2025-01-14 11:18:12 +0800

    update

---
+++ 01zerojudge/05 原創_不分類題庫/d486 Fibonacci 's computation process/v1-1.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+# python 3.12
+# ZeroJungle d486. Fibonacci 's computation process
+
+
+def fibonacci(n: int) -> list:
+    dp = [0] * (n + 1)
+    dp[0] = dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return dp
+
+
+def gen_fib_tree(nums: list):
+    print(' '.join([f'f({i})' for i in nums]))
+    if all(i < 2 for i in nums):
+        return
+    temp = []
+    for i in range(len(nums)):
+        if nums[i] < 2:
+            temp.append(nums[i])
+        else:
+            temp.extend([nums[i] - 1, nums[i] - 2])
+    return gen_fib_tree(temp)
+
+
+def main():
+    fib_list = fibonacci(15)
+    while True:
+        n = int(input())
+        if n == 0:
+            break
+        gen_fib_tree([n])
+        print(f'f({n}) = {fib_list[n]}', end='\n\n')
+
+
+if __name__ == '__main__':
+    main()
---
+++ 01zerojudge/05 原創_不分類題庫/d486 Fibonacci 's computation process/v2-1.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+# python 3.12
+# ZeroJungle d486. Fibonacci 's computation process
+
+
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def fibonacci(n: int) -> int:
+    if n < 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def gen_fib_tree(nums: list):
+    print(' '.join([f'f({i})' for i in nums]))
+    if all(i < 2 for i in nums):
+        return
+    temp = []
+    for i in range(len(nums)):
+        if nums[i] < 2:
+            temp.append(nums[i])
+        else:
+            temp.extend([nums[i] - 1, nums[i] - 2])
+    return gen_fib_tree(temp)
+
+
+def main():
+    while True:
+        n = int(input())
+        if n == 0:
+            break
+        gen_fib_tree([n])
+        print(f'f({n}) = {fibonacci(n)}', end='\n\n')
+
+
+if __name__ == '__main__':
+    main()
