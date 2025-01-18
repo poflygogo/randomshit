221b0ec0da00a9e7c0525bcef0e47f4becd4948e
poflygogo <144988024+poflygogo@users.noreply.github.com>
2025-01-18 13:31:54 +0800

    update

---
+++ 01zerojudge/03 IOI_APCS/03 TOI/TOIP/2019-03/e969 大吃大喝 (Big eater)/v1-1.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+# python 3.12
+# ZeroJudge e969. 大吃大喝 (Big eater)
+# 2019-03 TOI 練習賽 新手組
+
+
+food_cost = [32, 55]  # apple pie, corn soup
+money, period, flag = map(int, input().split())
+
+if money < food_cost[flag]:
+    print('Wayne can\'t eat and drink.')
+
+time = 0
+while money >= food_cost[flag]:
+    money -= food_cost[flag]
+    print(
+        f'{time}:',
+        'Wayne',
+        ('eats an Apple pie,', 'drinks a Corn soup,')[flag],
+        'and now he',
+        f'has {money} dollar{"s" if money > 1 else ""}.' if money > 0 else 'doesn\'t have money.'
+    )
+    time += period
+    flag ^= 1
