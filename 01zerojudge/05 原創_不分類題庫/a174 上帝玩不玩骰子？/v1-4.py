31cd1b1f86fec3defffedc3e6507bf694dd8d0fb
poflygogo <144988024+poflygogo@users.noreply.github.com>
2025-01-28 22:57:14 +0800

    update: 誰再玩骰子我就打誰

---
+++ 01zerojudge/05 原創_不分類題庫/a174 上帝玩不玩骰子？/v1-4.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+# python 3.12
+# ZeroJudge a174. 上帝玩不玩骰子？
+
+
+from array import array
+
+
+def god_play_dice(k: int, m: int):
+    data = [array('I') for _ in range(m)]
+    for _ in range(k):
+        comm = input().split()
+        if comm[0] == '1':
+            if int(comm[1]) not in data[int(comm[1]) % m]:
+                data[int(comm[1]) % m].append(int(comm[1]))
+        elif comm[0] == '2':
+            if int(comm[1]) in data[int(comm[1]) % m]:
+                data[int(comm[1]) % m].remove(int(comm[1]))
+        else:
+            print(
+                '===== s =====',
+                '\n'.join(f'[{i:03d}]:' + ' -> '.join(map(str, sorted(j) + ['NULL'])) for i, j in enumerate(data)),
+                '===== e =====',
+                sep='\n'
+            )
+
+
+def main():
+    while True:
+        try:
+            k, m = map(int, input().split())
+        except EOFError:
+            break
+        else:
+            god_play_dice(k, m)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 01zerojudge/05 原創_不分類題庫/a175 撒旦玩不玩骰子？/v1-1.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+# python 3.12
+# ZeroJudge a175. 撒旦玩不玩骰子？
+
+
+def god_play_dice(k: int, m: int):
+    data = [set() for _ in range(m)]
+    for _ in range(k):
+        comm = input().split()
+        if comm[0] == '1':
+            data[int(comm[1]) % m].add(int(comm[1]))
+        elif comm[0] == '2':
+            if int(comm[1]) in data[int(comm[1]) % m]:
+                data[int(comm[1]) % m].remove(int(comm[1]))
+        else:
+            print(
+                '===== s =====',
+                '\n'.join(f'[{i:03d}]:' + ' -> '.join(map(str, sorted(j) + ['NULL'])) for i, j in enumerate(data)),
+                '===== e =====',
+                sep='\n'
+            )
+
+
+def main():
+    while True:
+        try:
+            k, m = map(int, input().split())
+        except EOFError:
+            break
+        else:
+            god_play_dice(k, m)
+
+
+if __name__ == '__main__':
+    main()
