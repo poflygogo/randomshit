d63e32d9f7a5523c0751d2219ce1b66dd0281b10
poflygogo <144988024+poflygogo@users.noreply.github.com>
2025-02-05 22:29:20 +0800

    update: e942. pC. 數字排列

--- 01zerojudge/06 未解之謎/e942 pC 數字排列/v2-1.py
+++ 01zerojudge/06 未解之謎/e942 pC 數字排列/v2-1.py
@@ -3,3 +3,21 @@
 # zerojudge e942. pC. 數字排列
 # 2009大學學測推甄申請二階
 
+
+def dfs(path: list, visit: set):
+    if visit and path and len(path) == n:
+        print(*path)
+        return
+    for i in nums:
+        if i not in visit:
+            path.append(i)
+            visit.add(i)
+            dfs(path, visit)
+            path.pop()
+            visit.remove(i)
+
+
+n = int(input())
+nums = list(map(int, input().split()))
+nums.sort()
+dfs([], set())
