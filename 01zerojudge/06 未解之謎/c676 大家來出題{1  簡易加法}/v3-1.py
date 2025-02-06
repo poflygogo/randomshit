12066b56bde9daf0e9ca7fb937a2523cd113742f
poflygogo <144988024+poflygogo@users.noreply.github.com>
2025-02-06 16:45:15 +0800

    update: c676

---
+++ 01zerojudge/06 未解之謎/c676 大家來出題{1  簡易加法}/v3-1.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+# python 3.12
+# ZeroJudge c676. 大家來出題 { 1: 簡易加法 }
+
+
+import random
+
+
+seen = set()
+cnt = 100
+while cnt > 0:
+    a = random.randint(1, int(1e6))
+    b = random.randint(1, int(1e6))
+    c = a + b
+    if c in seen:
+        continue
+    seen.add(c)
+    cnt -= 1
+    print(a, b, c)
