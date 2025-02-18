f17d6fba8b02a8be2e92074024aa6834aaecbb8a
poflygogo <144988024+poflygogo@users.noreply.github.com>
2025-02-18 13:06:34 +0800

    fix typo

--- 01zerojudge/01 基礎題庫/series_i/i177 小畫家 (Painter)/v2-2.py
+++ 01zerojudge/01 基礎題庫/series_i/i177 小畫家 (Painter)/v2-2.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 # python 3.12
 # ZeroJudge i177. 小畫家 (Painter)
-# 2022-04 TOI 練習賽 新手組 第一題
+# 2022-04 TOI 練習賽 潛力組 第一題
 
 
 max_row, max_col, x, y, z = map(int, input().split())
