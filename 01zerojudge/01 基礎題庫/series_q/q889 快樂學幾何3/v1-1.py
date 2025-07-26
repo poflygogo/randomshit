79700190ee650e8d278bdc2ca6a45357ae3235cf
poflygogo <144988024+poflygogo@users.noreply.github.com>
2025-07-26 23:35:18 +0800

    update: 添加解題思路

--- 01zerojudge/01 基礎題庫/series_q/q889 快樂學幾何3/v1-1.py
+++ 01zerojudge/01 基礎題庫/series_q/q889 快樂學幾何3/v1-1.py
@@ -2,6 +2,9 @@
 # python 3.12
 # ZeroJudge q889. 快樂學幾何3
 
+# 思路:
+# https://mindyourdecisions.com/blog/2024/12/11/challenging-problem-from-turkeys-university-entrance-exam/
+# https://www.youtube.com/watch?v=TyZCM76Sx4g
 
 while True:
     try:
