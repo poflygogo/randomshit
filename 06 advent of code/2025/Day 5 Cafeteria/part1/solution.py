fdfc9241257673efa69c243e22749e6e98a03d7c
poflygogo <144988024+poflygogo@users.noreply.github.com>
2025-12-05 14:51:50 +0800

    feat: add Advent of code 2025 Day 5

---
+++ 06 advent of code/2025/Day 5 Cafeteria/part1/solution.py
@@ -0,0 +1,52 @@
+# advent of code 2025
+# day 5 cafeteria
+# part 1
+# python 3.12
+
+import pathlib
+from typing import TextIO
+
+
+class Solution:
+    def __init__(self, input_file: TextIO):
+        self.fresh_foods: list[tuple[int, int]] = self.parse_range(input_file)
+
+    def solve(self) -> int:
+        count = 0
+        for line in input_file:
+            if not line.strip():
+                continue
+            if self.is_contained(int(line.strip())):
+                count += 1
+        return count
+
+    def parse_range(self, input_file: TextIO) -> list[tuple[int, int]]:
+        data: list[tuple[int, int]] = []
+        for line in input_file:
+            if not line.strip():
+                break
+            a, b = line.strip().split("-")
+            data.append((int(a), int(b)))
+
+        data.sort()
+        result = [data.pop(0)]
+        for i, j in data:
+            if i > result[-1][1] + 1:
+                result.append((i, j))
+            else:
+                result[-1] = (result[-1][0], max(result[-1][1], j))
+
+        return result
+
+    def is_contained(self, val: int) -> bool:
+        for i, j in self.fresh_foods:
+            if i <= val <= j:
+                return True
+        return False
+
+
+if __name__ == "__main__":
+    input_path = pathlib.Path(__file__).parent.parent / "test_case" / "00.in"
+    with open(input_path, "r") as input_file:
+        solution = Solution(input_file)
+        print(solution.solve())
